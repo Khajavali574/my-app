2b50673c46034dbc70010fef83617fd610bb4bf0
Khajavali574 <khajavali574@gmail.com>
2018-01-21 21:50:34 +0530

    updated with express

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-var express = require('express');
+var express = require('express'),
+    bodyparser = require('body-parser'),
+    path = require('path');
 
 var app = express();
 var port = process.env.port || 8080;
+
+app.set('view engine','ejs')
+app.set('viewa',path.join(__dirname,'views'));
+app.use(bodyparser());
+
+var todoitems = [
+    {id:1,desc:'First'},
+    {id:2,desc:'Second'},
+    {id:3,desc:'Third'}
+];
+
 app.listen(port,function(){
     console.log("Server is started on port number "+port)
 })
 app.get("/",function(req,res){
-    res.send("Welcome to node");
-    res.end();
+    //res.send("Simple Example for Express Framework!!");
+    res.render("index",{
+        title:'Welcome to Express !!',
+        items:todoitems
+    });
+});
+
+
+app.post("/add",function(req,res){
+    var newitem = req.body.newitem;
+    console.log("New item is :"+newitem);
+    todoitems.push({
+        id:todoitems.length +1,
+        desc:newitem
+    });
+    res.redirect("/");
 });
